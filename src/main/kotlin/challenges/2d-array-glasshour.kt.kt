d5d7899c88e5ef45d8d041455fd608e786238fd9
aakashbhusal <aakash.bhusal@8squarei.com>
2023-02-20 23:37:54 +0545

    day 4 challenge glasshour 2d array

---
+++ src/main/kotlin/challenges/2d-array-glasshour.kt.kt
@@ -0,0 +1,63 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun hourglassSum(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    var sumArrayData = Array<Array<Int>>(5) {
+        Array<Int>(5) { 0 }
+    }
+
+    for(i in 0 until arr.size){
+        for(j in 0 until arr[i].size){
+            if(i<4&&j<4){
+                sumArrayData[i][j]=
+                    arr[i][j]+arr[i][j+1]+arr[i][j+2]+ arr[i+1][j+1]+arr[i+2][j]+ arr             [i+2][j+1]+arr[i+2][j+2];
+            }
+        }
+    }
+    var maxSum:Int=sumArrayData[0][0];
+    for(i in 0..3){
+        for (j in 0..3){
+            if(sumArrayData[i][j]>=maxSum){
+                maxSum=sumArrayData[i][j]
+            }
+        }
+    }
+    return maxSum;
+}
+
+fun main(args: Array<String>) {
+
+    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+
+    for (i in 0 until 6) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = hourglassSum(arr)
+
+    println(result)
+}
